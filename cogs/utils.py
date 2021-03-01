430e190dc46d25bc405c5c030ee0dbc29e52e6fa
eunwoo1104 <61371424+eunwoo1104@users.noreply.github.com>
2021-03-01 13:13:26 +0900

    맞춤봇 기능 추가

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,10 +1,37 @@
 from discord.ext import commands
 from module import JBotClient, AuthorEmbed, EmbedColor
+from extlib import SpellChecker
 
 
 class Utils(commands.Cog):
     def __init__(self, bot: JBotClient):
         self.bot = bot
+        self.spell = SpellChecker()
+
+    @commands.command(name="맞춤법", description="맞춤법을 검사해줘요. 틀린 경우가 있으니 참고용으로만 사용해주세요.")
+    async def spell_check(self, ctx: commands.Context, *, text):
+        orig = text
+        changed = text
+        resp = await self.spell.parse_async(text)
+        if resp is None:
+            return await ctx.reply("맞춤법 오류가 없어요.")
+        for x in resp["errInfo"]:
+            orig_text = x["orgStr"]
+            changed_text = x["candWord"]
+            changed = changed.replace(orig_text, changed_text)
+        if changed == orig:
+            return await ctx.reply("맞춤법 오류가 없어요.")
+        for x in resp["errInfo"]:
+            orig_text = x["orgStr"]
+            orig = orig.replace(orig_text, f"[__{orig_text}__]")
+        embed = AuthorEmbed(ctx.author,
+                            title="문법 오류를 발견했어요.",
+                            timestamp=ctx.message.created_at,
+                            color=EmbedColor.NEGATIVE,
+                            display_footer=True)
+        embed.add_field(name="수정 전", value=orig, inline=False)
+        embed.add_field(name="수정 후", value=changed, inline=False)
+        await ctx.reply(embed=embed)
 
 
 def setup(bot):
